04e1dce0f91215188ead2579b666bed50b62f360
William West <wwest466@gmail.com>
2020-09-18 14:28:52 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -6,17 +6,6 @@ class Course
     @@all << self
   end
 
-  def get_page
-
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
-    #end
-  end
 
   def self.all
     @@all
